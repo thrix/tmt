cc68976e37a9fb0cbee2036d47f16cdecb0046dd
Miroslav Vadkerti <mvadkert@redhat.com>
2022-05-24 19:15:55 +0200

    Add schemas for plans
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- tests/unit/test_schemas.py
+++ tests/unit/test_schemas.py
@@ -28,15 +28,35 @@ def stories_schema():
 
 
 @pytest.fixture
+def plans_schema():
+    # TODO: tmt package shall provide a helper function
+    # for "load schemas for test/plan/story"
+    schema_file = os.path.join(SCHEMADIR, 'plan.yaml')
+    return YAML(typ="safe").load(open(schema_file, encoding="utf-8"))
+
+
+@pytest.fixture
 def schema_store():
     # TODO: tmt package shall provide a helper function
     # for "load schemas for test/plan/story"
     store = {}
 
-    for schema_name in ('common', 'core'):
+    for schema_name in (
+            'common', 'core',
+            'discover/fmf', 'discover/shell',
+            'execute/tmt', 'execute/upgrade',
+            'finish/ansible', 'finish/shell',
+            'prepare/ansible', 'prepare/install', 'prepare/shell',
+            'provision/artemis', 'provision/connect', 'provision/container',
+            'provision/local', 'provision/virtual',
+            'report/display', 'report/html'
+            ):
         schema_file = os.path.join(SCHEMADIR, f'{schema_name}.yaml')
-        schema = YAML(typ="safe").load(
-            open(schema_file, encoding="utf-8"))
+        schema = YAML(
+            typ="safe").load(
+            open(
+                schema_file,
+                encoding="utf-8"))
         store[schema['$id']] = schema
 
     return store
@@ -52,6 +72,12 @@ def test_validation_result(request, schema_store, tests_schema):
 def story_validation_result(request, schema_store, stories_schema):
     node = request.param.node
     return node.name, node.validate(stories_schema, schema_store)
+
+
+@pytest.fixture(params=tmt.Tree(ROOTDIR).plans())
+def plan_validation_result(request, schema_store, plans_schema):
+    node = request.param.node
+    return node.name, node.validate(plans_schema, schema_store)
 
 
 def test_tests_schema(test_validation_result):
@@ -70,3 +96,12 @@ def test_stories_schema(story_validation_result):
             print(error)
 
     assert result.result, f'Story {name} fails validation'
+
+
+def test_plans_schema(plan_validation_result):
+    name, result = plan_validation_result
+    if not result.result:
+        for error in result.errors:
+            print(error)
+
+    assert result.result, f'Plan {name} fails validation'
